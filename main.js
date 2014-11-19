1511a23aef4f7b9bd6856369b423c1e288a4855d
nepstein <ne2210@columbia.edu>
2014-11-19 16:25:06 -0500

    nearest k neighbors implemented

--- main.js
+++ main.js
@@ -16,6 +16,28 @@ var updateMax = function(val,arr){
     return max;
 };
 
+function mode(array)
+{
+    if(array.length === 0)
+      return null;
+    var modeMap = {};
+    var maxEl = array[0], maxCount = 1;
+    for(var i = 0; i < array.length; i++)
+    {
+      var el = array[i];
+      if(modeMap[el] === null)
+        modeMap[el] = 1;
+      else
+        modeMap[el]++;
+      if(modeMap[el] > maxCount)
+      {
+        maxEl = el;
+        maxCount = modeMap[el];
+      }
+    }
+    return maxEl;
+}
+
 
 var kNear = function(k){
   //PRIVATE
@@ -62,6 +84,10 @@ var kNear = function(k){
       }
 
     });
-    console.log('final', voteBloc);
+    var votes = [];
+    voteBloc.forEach(function(el){
+      votes.push(el.vote);
+    });
+    return mode(votes);
   };
 };
